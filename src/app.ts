be526d319fd13db57b67d1fe32b96323eb280c1d
Mayank <raomayank920@gmail.com>
2026-08-26 01:30:16 +0530

    created a registerUser api which is receiving the req.body data + created compose.yml file

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from "express"
 import authRouter from "./routes/auth.route"
 const app=express();
 
+app.use(express.json())
+
 app.use("/auth",authRouter)
 
 export default app
--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,6 +2,7 @@ import {Request,Response} from "express"
 
 export const registerUser=async(req:Request,res:Response)=>{
     const {username,password}=req.body
+    
+    return res.status(201).json({message:"username & password entered",username,password})
 
-    console.log(username,password);
 }
